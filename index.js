9c810c069a68976ea57ac64298610f7a3337f768
Roziboy04 <fxolboyev007@gmail.com>
2023-04-07 14:09:53 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+// let arr = [1,2,3]
+
+// function num(a) {
+
+//    return a[0] +a[1]
+// }
+// console.log(num(arr));
+
+
+
+
+// function num(a, b){
+//     return num
+//     console.log ( a , b )
+// }
+
+// let arr = [1,2,3]
+// console.log(arr[1])
+// let ar = [1,7,3]
+// console.log(ar[1])
+
+
+function num(a, b){
+    return num
+    console.log ( a , b )
+}
+
+let arrr = [1,2,3]
+console.log(arrr[0])
+let arrrr = [1,7,3]
+console.log(arrrr[0])
+
+
+
+
+
+// function num (a,b){
+//    if (a  + b == 6){
+//        return true
+//     }else if(a - b == 6){
+//         return true
+//     }if (a = b ==6){
+//         return true
+//     }else if (a==b){
+//         return false 
+//     }
+
+// }
+// console.log( num(10,4));
+
+
+
+
+// function name(a){
+//     return "hello" + a
+// }
+// console.log(name("Ruziboy"));
+
+
+
+
+
+
+// function num (a){
+//     return  a + a + a
+// }
+// console.log( num("gh,gh,gh"));
+
+
+
+
+// function num (a,b){
+//     return a-b
+// }
+// console.log(num(2023,1982));
